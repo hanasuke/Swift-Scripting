5138545e20c1b31923a565b27a48f2f41e46c940
naosuke <naosuke@live.jp>
2016-02-21 00:07:14 +0900

    create ls command
    create basic ls command (not able to use options)

---
+++ ls.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let args = Process.arguments.suffixFrom(0)
+
+let fileManager:NSFileManager = NSFileManager()
+
+let files = try? fileManager.contentsOfDirectoryAtPath(args[1])
+
+for filename in files! {
+    print(filename)
+}
