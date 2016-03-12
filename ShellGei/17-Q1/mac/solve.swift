ce36e0020b00e2104a249aa4c43a3dd734797ff6
naosuke <naosuke@live.jp>
2016-03-13 02:06:01 +0900

    add converting json function using NSJSONSerialization

--- ShellGei/17-Q1/mac/solve.swift
+++ ShellGei/17-Q1/mac/solve.swift
@@ -1,7 +1,11 @@
 import Foundation
 
+func sortKeyInDictionary(data: Dictionary<String, Array<String>>) -> Array<String> {
+    return data.keys.sort(<)
+}
+
 func outputText(data: Dictionary<String, Array<String>>) {
-    let sortedKeys: Array<String> = data.keys.sort(<)
+    let sortedKeys: Array<String> = sortKeyInDictionary(data)
 
     for skey in sortedKeys {
         for (key, values) in data {
@@ -14,6 +18,8 @@ func outputText(data: Dictionary<String, Array<String>>) {
 }
 
 func outputJSON(data:Dictionary<String, Array<String>>) {
+    let jsonData = try! NSJSONSerialization.dataWithJSONObject(data, options: [])
+    print(NSString(data: jsonData, encoding:NSUTF8StringEncoding)!)
 }
 
 var data: Dictionary< String, Array<String> > = [:]
@@ -35,3 +41,4 @@ while true {
 }
 
 outputText(data)
+outputJSON(data)
