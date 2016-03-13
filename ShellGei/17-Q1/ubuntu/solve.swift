0f3322d725d41fa2fbfe4a7b128526d1b32a8f9a
Naoki Hanakawa <naoki.hanakawa@gmail.com>
2016-03-14 07:51:00 +0900

    sync ubuntu script from mac script

--- ShellGei/17-Q1/ubuntu/solve.swift
+++ ShellGei/17-Q1/ubuntu/solve.swift
@@ -1,8 +1,12 @@
 import Glibc
 import Foundation
 
+func sortKeyInDictionary(data: Dictionary<String, Array<String>>) -> Array<String> {
+    return data.keys.sort(<)
+}
+
 func outputText(data: Dictionary<String, Array<String>>) {
-    let sortedKeys: Array<String> = data.keys.sort(<)
+    let sortedKeys: Array<String> = sortKeyInDictionary(data)
 
     for skey in sortedKeys {
         for (key, values) in data {
@@ -15,15 +19,23 @@ func outputText(data: Dictionary<String, Array<String>>) {
 }
 
 func outputJSON(data:Dictionary<String, Array<String>>) {
+    //let jsonData = try! NSJSONSerialization.dataWithJSONObject(data, options: [])
+    //print(NSString(data: jsonData, encoding:NSUTF8StringEncoding) as! String)
 }
 
 var data: Dictionary< String, Array<String> > = [:]
 
-while true {
-    var line = readLine()!
-    var parseData = line.componentsSeparatedByString(" ")
+let args = Process.arguments.suffixFrom(0)
+let option = args[1]
+let filePath = args[2]
 
-    if ( line == "" ) {
+let fileData = try! NSString(contentsOfFile: filePath, encoding: NSUTF8StringEncoding)
+let lineData = fileData.componentsSeparatedByString("\n")
+
+for var i in 0..<lineData.count {
+    var parseData = lineData[i].componentsSeparatedByString(" ")
+
+    if ( lineData[i] == "" ) {
         break
     }
 
@@ -32,7 +44,15 @@ while true {
     } else {
         data[parseData[0]]!.append(String(parseData[1]))
     }
-
 }
 
-outputText(data)
+switch option {
+case "-json" :
+    outputJSON(data)
+    break
+case "-text" :
+    outputText(data)
+    break
+default :
+    print("argument error")
+}
